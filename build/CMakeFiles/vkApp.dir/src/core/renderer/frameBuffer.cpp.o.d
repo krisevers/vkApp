50a9285ce9280bb2efd8063df4dbc3ec648e576b
Kris Evers <krisevers14@gmail.com>
2025-03-15 16:41:29 +0100

    update git index

--- build/CMakeFiles/vkApp.dir/src/core/renderer/frameBuffer.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/vkApp.dir/src/core/renderer/frameBuffer.cpp.o: \
- /home/kris/Projects/vkApp/src/core/renderer/frameBuffer.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/vkApp.dir/src/core/renderer/mesh.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/vkApp.dir/src/core/renderer/mesh.cpp.o: \
- /home/kris/Projects/vkApp/src/core/renderer/mesh.cpp \
- /usr/include/stdc-predef.h
